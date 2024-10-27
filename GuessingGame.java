304282acb11a2c2c6e1a38de31f15c91434d96db
emmi0122 <emiliaidajarleback@gmail.com>
2024-10-27 14:43:40 +0100

    Adding main class

---
+++ GuessingGame.java
@@ -0,0 +1,9 @@
+public class GuessingGame {
+    public static void main(String[] args) {
+        // Skapar en instans av Guesser med intervallet 0 till 1000
+        Guesser guesser = new Guesser(0, 1000);
+
+        // Just nu gör vi inget mer eftersom Guesser-klassen inte är klar än
+        System.out.println("Guesser object created successfully.");
+    }
+}
